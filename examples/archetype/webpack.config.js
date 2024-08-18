539c44593b62f3b870b72b4a374b365fcba64a66
Andrei Gatej <andreigtj01@gmail.com>
2024-08-18 22:43:28 +0300

    chore: update archetype and webpack

--- examples/archetype/webpack.config.js
+++ examples/archetype/webpack.config.js
@@ -1,14 +1,16 @@
-const path = require('path');
+const path = require("path");
 
 /**
  * @type {import("../../webpack/declarations/WebpackOptions").WebpackOptions}
  */
 const config = {
+  // context: path.join(__dirname),
   entry: path.join(__dirname),
   output: {
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, "dist"),
+    clean: true,
   },
-  mode: 'none'
+  mode: "none",
 };
 
 module.exports = config;
